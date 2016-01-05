d187efd2388821c10b08f38b833e90532471b039
ChrisWphoto <walter.chris@gmail.com>
2016-01-05 16:57:32 -0600

    Initial Commit

---
+++ Led-Grid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Led-Grid
+//
+//  Created by Christopher G Walter on 1/5/16.
+//  Copyright © 2016 Christopher G Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
